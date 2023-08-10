a572820cab8fcec5b3b7cc15b6c68c94bb4ff649
John Mortensen <jmortensen@powayusd.com>
2023-08-10 07:08:36 -0700

    activate script

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,48 +1,16 @@
 ## Terminal Commands
-These commands validate serve capbilities
-
 #### Installs ruby and gnu tools
-```bash
 echo "=== Missing Packages ==="
-sudo apt installl ruby-full build-essential
-```
-
-#### Puts (base) in prompt
-```bash
-echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
-echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
-echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
-
-echo "=== Make Conda environment (base) ==="
-conda activate
-```
+sudo apt -y install ruby-full build-essential
 
 #### Github Pages Local Build
-```bash
 echo "=== GitHub pages build tools  ==="
+export GEM_HOME="$HOME/gems"
+export PATH="$HOME/gems/bin:$PATH"
+
 gem install jekyll bundler
-```
 
 ### Project Requirements
-```bash
 cd ~
 git clone https://github.com/nighthawkcoders/student
 cd ~/student
-```
-
-#### Ruby Gems for project
-
-```bash
-bundle install
-``` 
-
-#### Run Webserver
-
-```bash
-bundle exec jekyll serve
-```
-
-
-
-
-
