02181c2850e4fe6fd7f300d4a954e2faccf46b92
Safin Singh <safin.singh@gmail.com>
2023-10-13 12:36:09 -0700

    Layer -> GameObject

--- assets/js/layer.js
+++ assets/js/layer.js
@@ -1,16 +1,16 @@
-class Layer {
+class GameObject {
     constructor(canvas2D/* ADD STUFF HERE
-    add some parameters here to customize layer (maybe width, height, color, x/y position, etc.) */) {
+    add some parameters here to customize game object (maybe width, height, color, x/y position, etc.) */) {
         this.c = canvas2D;
         /* ADD STUFF HERE
      set appropriate properites here */
     }
     update() {
         /* ADD STUFF HERE
-     draw this layer on the canvas*/
+     draw this object on the canvas*/
     }
    isCollidingWith(player) {
        /* ADD STUFF HERE (PART 2)
-     (part 2) - check if this layer is colliding with the player, return true or false */
+     (part 2) - check if this game object is colliding with the player, return true or false */
    }
 }
